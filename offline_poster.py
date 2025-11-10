09eddce1b6c3b8c51e74ec082804f4270d684284
iron-prog <dt915725@gmail.com>
2025-11-10 21:58:38 +0530

    Add files via upload

---
+++ offline_poster.py
@@ -0,0 +1,15 @@
+import pickle, requests, pandas as pd, time
+
+API_KEY = 'd9a5e1812b867e932d0efd41c90dd95a'
+movie = pickle.load(open('/Users/deepaktiwari/Downloads/movies.pkl', 'rb'))
+
+def fetch_poster_url(movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
+        data = requests.get(url, timeout=10).json()
+        return "https://image.tmdb.org/t/p/w500/" + data['poster_path'] if data.get('poster_path') else None
+    except:
+        return None
+
+movie['poster_url'] = movie['movie_id'].apply(fetch_poster_url)
+movie.to_pickle('movies_with_posters.pkl')
